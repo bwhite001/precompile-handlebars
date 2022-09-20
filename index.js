cbadf83cc169056867211c332d8fb249989b4cc7
Brandon White <bwhite@infoxchange.org>
2022-09-20 16:28:37 +1000

    Added Testing update 4 files, delete 3 files, move 4 files and create 1 file

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 let mix = require("laravel-mix");
+const HandlebarTask = require("./task");
 
 class LaravelMixHandlebars {
     dependencies() {
         this.requiresReload = true;
-        return ['glob', 'handlebars']
+        return ["fs","path","handlebars"];
     }
     name () {
-        return 'Handlebars';
+        return "Handlebars";
     }
 
     register(inputDir, outputFile) {
-        this.config =
+        const config =
             {
                 inputDir: inputDir,
                 outputFile: outputFile
             };
-        const HandlebarTask = require('./task');
-        Mix.addTask(
-            new HandlebarTask(this.config)
-        )
-    }
+        Mix.api.before(() => {
+            let task = new HandlebarTask(config);
+            task.run();
+        })
+        Mix.api.js(config.outputFile, config.outputFile);
 
+    }
 }
 
-mix.extend('handlebars', new LaravelMixHandlebars());
+mix.extend("handlebars", new LaravelMixHandlebars());
--- task.js
+++ task.js
@@ -1,17 +1,17 @@
-const Task = require('laravel-mix/src/tasks/Task');
+const Task = require("laravel-mix/src/tasks/Task");
 const path = require("path");
 const fs = require("fs");
-let Log = require('laravel-mix/src/Log');
+let Log = require("laravel-mix/src/Log");
 
-const preFile = 'this["App"] = this["App"] || {};\n\
-this["App"]["templates"] = this["App"]["templates"] || {};\n\n';
-const preTemplate1 = 'this["App"]["templates"]["';
-const preTemplate2 = '"] = Handlebars.template(';
-const postTemplate = ');\n\n';
+const preFile = "this[\"App\"] = this[\"App\"] || {};\n\
+this[\"App\"][\"templates\"] = this[\"App\"][\"templates\"] || {};\n\n";
+const preTemplate1 = "this[\"App\"][\"templates\"][\"";
+const preTemplate2 = "\"] = Handlebars.template(";
+const postTemplate = ");\n\n";
 
-const prePartialTemplate1 = 'Handlebars.registerPartial("';
-const prePartialTemplate2 = '", Handlebars.template(';
-const postPartial = '));\n\n';
+const prePartialTemplate1 = "Handlebars.registerPartial(\"";
+const prePartialTemplate2 = "\", Handlebars.template(";
+const postPartial = "));\n\n";
 
 class HandlebarTask extends Task {
 
@@ -27,43 +27,39 @@ class HandlebarTask extends Task {
     precompile() {
         const params = this.data;
         const cwd = process.cwd();
-
-        this.src = cwd + '/' + params.inputDir;
-        this.dist_file = cwd + '/' + params.outputFile;
-        let outputDirectory = this.dist_file.match(/(.*)\/.*/)[1];
+        this.src = path.resolve(params.inputDir);
+        this.dist_file = path.resolve(params.outputFile);
+        let outputDirectory = path.dirname(this.dist_file);
         this.variables = params.variables;
         if (!fs.existsSync(outputDirectory)) {
             fs.mkdirSync(outputDirectory, {recursive: true});
         }
-        this.Handlebars = require('handlebars');
-        this.glob = require('glob');
-
-        process.chdir(this.src)
+        else if (fs.existsSync(this.dist_file)) {
+            fs.rmSync(this.dist_file);
+        }
+        this.Handlebars = require("handlebars");
+        this.glob = require("glob");
+        process.chdir(this.src);
         this.compile();
+        fs.writeFileSync(this.dist_file, this.file_string);
         process.chdir(cwd);
     }
 
     compile() {
-        const files = this.glob.sync(this.src + '/' + '/^.*\\.hbs/');
-        try {
-            if(!fs.existsSync(this.dist_file)) {
-                fs.writeFileSync(this.dist_file, preFile)
-            }
-        } catch(e) {
-            throw new Error(
-                `could not create file at ${this.dist_file}: ${e.message || e}`
-            );
-        }
-        fs.writeFileSync(this.dist_file, preFile)
+        this.file_string = preFile;
+        const files = fs.readdirSync(this.src);
         for (const file of files) {
             try {
-                let fileName = file, shortFileName = fileName.match(/(.*)\..*/)[1];
-                let input = path.join(this.data.inputDir, fileName);
+                if(path.extname(file) != ".hbs"){
+                    continue;
+                }
+                let fileName = file, shortFileName = path.parse(file).name;
+                let input = path.join(this.src, fileName);
                 if (!fs.lstatSync(input).isFile()) {
                     continue;
                 }
-                let content = fs.readFileSync(input, 'utf8');
-                this.saveFile(shortFileName, content)
+                let content = fs.readFileSync(input, "utf8");
+                this.file_string += this.addTemplate(shortFileName, content);
             } catch (e) {
                 throw new Error(
                     `could not render template ${file}: ${e.message || e}`
@@ -72,15 +68,25 @@ class HandlebarTask extends Task {
         }
     }
 
-    saveFile(shortFileName, data) {
+    addTemplate(shortFileName, data) {
         let templateSpec = this.Handlebars.precompile(data);
-        if (shortFileName[0] === '_') {
+        if (shortFileName[0] === "_") {
             // handling partial
             templateSpec = prePartialTemplate1 + shortFileName.slice(1) + prePartialTemplate2 + templateSpec + postPartial;
         } else {
             templateSpec = preTemplate1 + shortFileName + preTemplate2 + templateSpec + postTemplate;
         }
-        fs.appendFileSync(this.dist_file, templateSpec);
+        return templateSpec;
+    }
+
+    saveToOutputFile(content) {
+        try {
+            fs.writeFileSync(this.dist_file, content);
+        } catch(e) {
+            throw new Error(
+                `could not save file at ${this.dist_file}: ${e.message || e}`
+            );
+        }
     }
 }
 
